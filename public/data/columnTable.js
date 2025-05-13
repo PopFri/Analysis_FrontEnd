568d4a2bd85ce56e79abbed022c801f7c55b7df4
penep0 <>
2025-05-13 15:08:36 +0900

    [FEAT] #10 디자인 디테일 수정

--- public/data/columnTable.js
+++ public/data/columnTable.js
@@ -135,186 +135,7 @@ export const resultData = {
                 "column_visit": "33",
                 "user_data": "1503",
             },
-            {
-                "idvisit": "34",
-                "isvisitisvisit": "5431",
-                "time_spent_ref_action": "5431",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "42",
-                "isvisitisvisit": "15",
-                "time_spent_ref_action": "15",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "51",
-                "isvisitisvisit": "1908",
-                "time_spent_ref_action": "1908",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "32",
-                "isvisitisvisit": "1000",
-                "time_spent_ref_action": "1000",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "22",
-                "isvisitisvisit": "1434",
-                "time_spent_ref_action": "1434",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "34",
-                "isvisitisvisit": "5431",
-                "time_spent_ref_action": "5431",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "42",
-                "isvisitisvisit": "15",
-                "time_spent_ref_action": "15",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "51",
-                "isvisitisvisit": "1908",
-                "time_spent_ref_action": "1908",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "32",
-                "isvisitisvisit": "1000",
-                "time_spent_ref_action": "1000",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "22",
-                "isvisitisvisit": "1434",
-                "time_spent_ref_action": "1434",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "34",
-                "isvisitisvisit": "5431",
-                "time_spent_ref_action": "5431",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "42",
-                "isvisitisvisit": "15",
-                "time_spent_ref_action": "15",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "51",
-                "isvisitisvisit": "1908",
-                "time_spent_ref_action": "1908",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "32",
-                "isvisitisvisit": "1000",
-                "time_spent_ref_action": "1000",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "22",
-                "isvisitisvisit": "1434",
-                "time_spent_ref_action": "1434",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "34",
-                "isvisitisvisit": "5431",
-                "time_spent_ref_action": "5431",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "42",
-                "isvisitisvisit": "15",
-                "time_spent_ref_action": "15",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            },
-            {
-                "idvisit": "51",
-                "isvisitisvisit": "1908",
-                "time_spent_ref_action": "1908",
-                "asdf": "1111",
-                "ieie": "939",
-                "table_tata": "2929",
-                "column_visit": "33",
-                "user_data": "1503",
-            }
+            
         ]
         
     }
@@ -342,110 +163,171 @@ export const resultRecordData = {
             { condition: "isvisitisvisit == 9 && isvisit > 5", count: 780 },
             { condition: "isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)", count: 780 }
           ]
-        }
+        },
+        {
+            date: "2025.01.01 13:41",
+            dataList: [
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9", count: 780 },
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 && isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)", count: 780 }
+            ]
+          },
+          {
+            date: "2025.01.01 14:00",
+            dataList: [
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9", count: 780 },
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 && isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)", count: 780 }
+            ]
+          },
+          {
+            date: "2025.01.01 13:41",
+            dataList: [
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9", count: 780 },
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 && isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)", count: 780 }
+            ]
+          },
+          {
+            date: "2025.01.01 14:00",
+            dataList: [
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9", count: 780 },
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 && isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)", count: 780 }
+            ]
+          },
+          {
+            date: "2025.01.01 13:41",
+            dataList: [
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9", count: 780 },
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 && isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)", count: 780 }
+            ]
+          },
+          {
+            date: "2025.01.01 14:00",
+            dataList: [
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9", count: 780 },
+              { condition: "isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 && isvisit > 5", count: 780 },
+              { condition: "isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)", count: 780 }
+            ]
+          }
       ]
     }
 };
 
-export const graphData = [
-    {
-      name: 'isvisit > 5',
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'isvisitisvisit == 9',
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'isvisitisvisit == 9 && isvisit > 5',
-      pv: 2800,
-      amt: 2290,
-    },
-    {
-      name: 'isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)',
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-        name: 'isvisit > 5',
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'isvisitisvisit == 9',
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'isvisitisvisit == 9 && isvisit > 5',
-        pv: 2800,
-        amt: 2290,
-    },
-    {
-        name: 'isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)',
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: 'isvisit > 5',
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'isvisitisvisit == 9',
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'isvisitisvisit == 9 && isvisit > 5',
-        pv: 2800,
-        amt: 2290,
-    },
-    {
-        name: 'isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)',
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: 'isvisit > 5',
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'isvisitisvisit == 9',
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'isvisitisvisit == 9 && isvisit > 5',
-        pv: 2800,
-        amt: 2290,
-    },
-    {
-        name: 'isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)',
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: 'isvisit > 5',
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'isvisitisvisit == 9',
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'isvisitisvisit == 9 && isvisit > 5',
-        pv: 2800,
-        amt: 2290,
-    },
-    {
-        name: 'isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)',
-        pv: 3908,
-        amt: 2000,
-    },
-];
+export const graphData = {
+    "total" : "176,040",
+    "data" : [
+        {
+            name: 'isvisit > 5',
+            pv: 2400,
+        },
+        {
+            name: 'isvisitisvisit == 9',
+            pv: 1398,
+        },
+        {
+            name: 'isvisitisvisit == 9 && isvisit > 5',
+            pv: 2800,
+            amt: 2290,
+        },
+        {
+            name: 'isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)',
+            pv: 3908,
+            amt: 2000,
+        },
+        {
+            name: 'isvisit > 5',
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: 'isvisitisvisit == 9',
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: 'isvisitisvisit == 9 && isvisit > 5',
+            pv: 2800,
+            amt: 2290,
+        },
+        {
+            name: 'isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)',
+            pv: 3908,
+            amt: 2000,
+        },
+        {
+            name: 'isvisit > 5',
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: 'isvisitisvisit == 9',
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: 'isvisitisvisit == 9 && isvisit > 5',
+            pv: 2800,
+            amt: 2290,
+        },
+        {
+            name: 'isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)',
+            pv: 3908,
+            amt: 2000,
+        },
+        {
+            name: 'isvisit > 5',
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: 'isvisitisvisit == 9',
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: 'isvisitisvisit == 9 && isvisit > 5',
+            pv: 2800,
+            amt: 2290,
+        },
+        {
+            name: 'isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)',
+            pv: 3908,
+            amt: 2000,
+        },
+        {
+            name: 'isvisit > 5',
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: 'isvisitisvisit == 9',
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: 'isvisitisvisit == 9 && isvisit > 5',
+            pv: 2800,
+            amt: 2290,
+        },
+        {
+            name: 'isvisitisvisit == 9 || (isvisit > 5 && time_spent_ref_action < 10)',
+            pv: 3908,
+            amt: 2000,
+        }
+    ]
+};
--- src/components/filteringResult/FilteringResultGraph.jsx
+++ src/components/filteringResult/FilteringResultGraph.jsx
@@ -1,12 +1,12 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { graphData } from '../../../public/data/columnTable';
 import '../../styles/filteringResult/FilteringResultGraph.css'
-import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const FilteringResultGraph = () => {
     const data = graphData;
     const CustomizedAxisTick = ({ x, y, payload }) => {
-        const MAX_LENGTH = 10; // ✅ 최대 표시 글자 수
+        const MAX_LENGTH = 7; //  최대 표시 글자 수
         const text = payload.value.length > MAX_LENGTH
           ? payload.value.slice(0, MAX_LENGTH) + '...'
           : payload.value;
@@ -19,7 +19,7 @@ const FilteringResultGraph = () => {
               dy={16}
               textAnchor="middle"
               fill="#ffffff"
-              fontSize={12}
+              fontSize={10}
             >
               {text}
             </text>
@@ -27,31 +27,38 @@ const FilteringResultGraph = () => {
         );
     };
     const barSize = 40;
-    const chartWidth = data.length * barSize + data.length * 10; // 간격 포함한 넉넉한 너비
+    const barGap = 10;
+    const chartWidth = data.data.length * (barSize + barGap) + 90;
     return (
         <div className='filtering-result-graph-container'>
             <div className='result-graph-wrapper'>
                 <div className='result-graph-title'>
                     <p className='result-graph-title-text'>필터링 결과 그래프</p>
                 </div>
+                <div className='result-total-data'>
+                    (전체 데이터 수: {data.total})
+                </div>
                 <div className='result-graph'>
-                    <BarChart
-                    width={chartWidth}
-                    height={300}
-                    data={data}
-                    margin={{
-                        top: 5,
-                        right: 20,
-                        left: 10,
-                        bottom: 30,
-                    }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" tick={<CustomizedAxisTick />} interval={0} />
-                        <YAxis tick={{ fill: '#1ED863', fontSize: 12 }} />
-                        <Tooltip />
-                        <Bar dataKey="pv" fill="#1ED863" activeBar={<Rectangle fill="white" stroke="green" />} barSize={30} />
-                    </BarChart>
+                    <div style={{ width: chartWidth }}>
+                        <BarChart
+                        width={chartWidth}
+                        height={260}
+                        data={data.data}
+                        margin={{ top: 10, right: 10, left: 10, bottom: 0 }}
+                        barCategoryGap={barGap}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" tick={<CustomizedAxisTick />} interval={0} />
+                            <YAxis tick={{ fill: '#1ED863', fontSize: 12 }} />
+                            <Tooltip />
+                            <Bar
+                                dataKey="pv"
+                                fill="#1ED863"
+                                barSize={barSize} // ✅ 고정 크기
+                                activeBar={<Rectangle fill="white" stroke="green" />}
+                            />
+                        </BarChart>
+                    </div>
                 </div>
             </div>
         </div>
