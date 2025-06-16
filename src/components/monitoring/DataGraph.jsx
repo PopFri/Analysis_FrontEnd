4704216c83f7518c2d7a4be694f9855b3ceca011
penep0 <>
2025-06-16 14:03:04 +0900

    [FEAT] #17 방문자 수 그래프 구현

--- src/components/monitoring/DataGraph.jsx
+++ src/components/monitoring/DataGraph.jsx
@@ -1,15 +1,67 @@
-import React from 'react'
-import '../../styles/monitoring/Overview.css'
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import '../../styles/monitoring/DataGraph.css'
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+  },
+];
 
 const DataGraph = () => {
 
     return (
-        <div className='overview-container'>
-            <div className='overview-list'>
-                <div className='overview-title'>
-                    <p className='overview-title-text'>분석 컬럼</p>
+        <div className='datagraph-container'>
+            <div className='datagraph-list'>
+                <div className='datagraph-title'>
+                    <p className='datagraph-title-text'>방문자 수</p>
                 </div>
-                
+                <div className='data-graph'>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <LineChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                        >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" tick={{ fill: '#FFFFFF', fontSize: 12 }}/>
+                        <YAxis tick={{ fill: '#FFFFFF', fontSize: 12 }}/>
+                        <Tooltip />
+                        <Line type="monotone" dataKey="uv" stroke="#1ED863" activeDot={{ r: 3 }} />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
             </div>
         </div>
     )
--- src/components/monitoring/Overview.jsx
+++ src/components/monitoring/Overview.jsx
@@ -7,7 +7,7 @@ const Overview = () => {
         <div className='overview-container'>
             <div className='overview-list'>
                 <div className='overview-title'>
-                    <p className='overview-title-text'>분석 컬럼</p>
+                    <p className='overview-title-text'>Overview</p>
                 </div>
                 
             </div>
