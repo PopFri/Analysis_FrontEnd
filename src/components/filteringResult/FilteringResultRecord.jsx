c6e1112755ecf994ecc9fa37222b7a6ae5377e9a
penep0 <>
2025-06-26 14:44:40 +0900

    [FEAT] 코드 롤백

--- src/components/filteringResult/FilteringResultRecord.jsx
+++ src/components/filteringResult/FilteringResultRecord.jsx
@@ -9,35 +9,34 @@ const FilteringResultRecord = ({resultData}) => {
                     <p className='result-record-title-text'>필터링 결과 기록</p>
                 </div>
                 <div className="result-record-table">
-                    <div className="record-title">resultData</div>
-                    <div  className="record-table-wrapper"> 
-                        <div className="record-group">
-                            <div className="record-table-row"> 
-                                <div className="record-header-condition">
-                                    CONDITION
-                                </div>
-                                <div className="record-header-count">
-                                    TRUE
-                                </div>
-                                <div className="record-header-count">
-                                    FALSE
-                                </div>
+                    <div className="record-title">resultData</div> 
+                    <div className="record-group">
+                        <div className="record-table-row"> 
+                            <div className="record-header-condition">
+                                CONDITION
+                            </div>
+                            <div className="record-header-count">
+                                TRUE
+                            </div>
+                            <div className="record-header-count">
+                                FALSE
                             </div>
                         </div>
-                        {resultData.map((data, index) => (
-                            <div className="record-table-row"key={index}> 
-                                <div className="record-table-condition">
-                                    {data.condition}
-                                </div>
-                                <div className="record-table-count">
-                                    {data.successCount}
-                                </div>
-                                <div className="record-table-count">
-                                    {data.failedCount}
-                                </div>
-                            </div>
-                        ))}
                     </div>
+                    {resultData.map((data, index) => (
+                        <div className="record-table-row"key={index}> 
+                            <div className="record-table-condition">
+                                {data.condition}
+                            </div>
+                            <div className="record-table-count">
+                                {data.successCount}
+                            </div>
+                            <div className="record-table-count">
+                                {data.failedCount}
+                            </div>
+                        </div>
+
+                    ))}
                 </div>
             </div>
         </div>
